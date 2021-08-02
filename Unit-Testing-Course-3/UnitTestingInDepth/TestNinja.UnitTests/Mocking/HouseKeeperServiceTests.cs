fd6c55cf18725d3975a43714b39ff2037255f123
Demian159 <adrian_salatino@hotmail.com>
2021-08-02 18:11:46 -0300

    update

---
+++ Unit-Testing-Course-3/UnitTestingInDepth/TestNinja.UnitTests/Mocking/HouseKeeperServiceTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class HouseKeeperServiceTests
+    {
+        [Test]
+        public void SendStatementEmails_WhenCalled_GenerateStatements()
+        {
+            var unitOfWork = new Mocking<IUnitOfWork>();
+            unitOfWork.
+        }
+    }
+}
